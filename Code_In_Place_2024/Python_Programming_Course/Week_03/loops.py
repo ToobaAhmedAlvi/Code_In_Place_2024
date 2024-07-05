73bd510fa8403865b6ee0db0fb13319276f95e4d
ToobaAhmedAlvi <alvi.tooba@gmail.com>
2024-07-05 15:16:30 +0500

    added week 3 content

---
+++ Code_In_Place_2024/Python_Programming_Course/Week_03/loops.py
@@ -0,0 +1,12 @@
+# Iterating over a String
+print("String Iteration")
+
+s = "Geeks"
+for i in s:
+    print(i)
+
+
+l1 = ["eat", "sleep", "repeat"]
+
+for count, ele in enumerate(l1):
+    print (count, ele)
