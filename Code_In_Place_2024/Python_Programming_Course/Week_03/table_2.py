5434d5c93e8ef87403b09531c72b46cd890fc6a4
ToobaAhmedAlvi <alvi.tooba@gmail.com>
2024-07-13 10:52:45 +0500

    added table of 2

--- Code_In_Place_2024/Python_Programming_Course/Week_03/table_2.py
+++ Code_In_Place_2024/Python_Programming_Course/Week_03/table_2.py
@@ -1,7 +1,12 @@
 def main():
+    table_of_two()
     
 def table_of_two():
-    pass
+    for i in range(10):
+        n=i+1
+        print(2, '*',n,"=",2*n)
+
+        
 if __name__ == '__main__':
     main()
 
