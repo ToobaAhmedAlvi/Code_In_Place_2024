bc94120f002ff081a32e878b67e9ec54a41229d8
ToobaAhmedAlvi <alvi.tooba@gmail.com>
2024-07-20 08:37:26 +0500

    added table 5

--- Code_In_Place_2024/Python_Programming_Course/Week_03/assignment_q2_prime_number.py
+++ Code_In_Place_2024/Python_Programming_Course/Week_03/assignment_q2_prime_number.py
@@ -1,12 +1,26 @@
 #prime number is only divisible by itself and 1
 #whole numbers 0-infinity
 #15 --> 1,3,5,15
-
+def prime_number(number):
+    if number<=1:
+        print(number,"Not a prime number
+        ")
+    elif number>1:
+        for i in range(2,number):
+            if number%i==0:
+                print(number,"Not a prime number")    
+                break     
     
-
+    elif number%i!=0:
+        print(number,"a prime number")
+    else:
+        print("a prime number")         
+          
+    return number
+    
 def main():
-    number= int(input("Enter a number to check:\n"))
-    print(number)
+    number=int(input("Enter a number to check:"))
+    prime_number(number)
     
-if __name__ == '__main__':
+if __name__ =='__main__':
     main()
--- Code_In_Place_2024/Python_Programming_Course/Week_04/index_game.py
+++ Code_In_Place_2024/Python_Programming_Course/Week_04/index_game.py
@@ -1,5 +1,26 @@
+import random
+
 def main():
-    pass
+    ##game=[0,1,2,3,4]
+    game=['Julie','Mehran','Simba','Ayesha','Karel']
+    max_index=len(game)-1
+    #print(max_index)
+    guess_index=random.randint(0,max_index)
+    #print(guess_index)
+    correct_string=game[guess_index]
+    #print(correct_string)
+    print("Guess the value from the following list\n")
+    for i in game:
+        print(i)
+    user_input=input("Enter a guessing string from the above:\n ")
+    
+    if user_input==correct_string:
+        print("Correct Answer")
+    else:
+        print("Wrong Answer.The correct answer is",correct_string)
+
+
+    
 
 
 if __name__=='__main__':
